96e2e626ad1117e46cb048b5dbeb6ec490365099
AliceMozyrko <mozyrkoalisa@gmail.com>
2024-08-31 12:27:11 +0300

    finish

--- src/server.js
+++ src/server.js
@@ -22,17 +22,6 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cors());
 
-
-    app.use('*', (req, res) => {
-      res.status(404).json({
-        message: 'Not found',
-      });
-    }),
-
-    app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    });
-
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
@@ -46,23 +35,36 @@ export const startServer = () => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
-	if (!contact) {
-	  res.status(404).json({
-		  message: 'Contact not found'
-	  });
-	  return;
-	}
+  if (!contact) {
+    res.status(404).json({
+      message: 'Contact not found'
+    });
+    return;
+  }
 
     res.status(200).json({
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
   });
-
-    app.get('/contacts', async (req, res) => {
-
-  });
-
-    app.get('/contacts/:contactId', async (req, res) => {
 });
+
+
+  app.get('/contacts', async (req, res) => {
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+  });
+
+    app.use('*', (req, res) => {
+      res.status(404).json({
+        message: 'Not found',
+      });
+    }),
+
+    app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
+
+
+
 };
